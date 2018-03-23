fb0c4d738fa9b6beddb9e6cf8c82b89f643e68c9
Reza Aghaei <rag@sitecore.net>
2018-03-23 16:41:12 +0800

    Added readme.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tab Control with Close Button and Add Button
+You can show close button on tab pages of a tab control to let the users close a tab. Also you can show an add button for tab control to let the users add a new tab.
+
+![tabpage close button and add button](image.png)
+
+To do so, We will use an owner draw tab to show close icons on each tab an add icon on the last tab. We use `DrawItem` to draw close and add icons, `MouseDown` to handle click on close and add buttons, `Selecting` to prevent selecting of the last tab and `HandleCreated` to adjust tab width.
+
+You need to set a suitable padding and set `DrawMode` to `OwnerDrawFixed` and also set the text of last tab page to empty string. We will use that tab as add button. 
+
+**Handle click on close button and add button**
+
+You can handle `MouseDown` or `MouseClick` event and check if the last tab rectangle contains the mouse clicked point, then insert a tab before the last tab. Otherwose check if one of close buttons contains clicked location, then close the tab which its close button was clicked
+
+**Prevent selectin last tab**
+
+To prevent selection the last tab, you can handle `Selecting` event of control and check if the selecting tab is the last tab, cancel the event.
+
+**Draw Close Button and Add Button**
+
+To draw close button and add button, you can handle `DrawItem` event. 
+
+**Adjust Tab width**
+
+Set the last tab text to empty string. Then to adjust tab width and let the last tab have smaller width, you can hanlde `HandleCreated` event and send a [`TCM_SETMINTABWIDTH `](https://msdn.microsoft.com/en-us/library/windows/desktop/bb760637(v=vs.85).aspx) to the control and specify the minimum size allowed for the tab width
---
+++ src/TabControlWithCloseButtonAndAddButton/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabControlWithCloseButtonAndAddButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.tabControl1.TabPages[this.tabControl1.TabCount - 1].Text = "";
+            this.tabControl1.Padding = new Point(12, 4);
+            this.tabControl1.DrawMode = TabDrawMode.OwnerDrawFixed;
+
+            this.tabControl1.DrawItem += tabControl1_DrawItem;
+            this.tabControl1.MouseDown += tabControl1_MouseDown;
+            this.tabControl1.Selecting += tabControl1_Selecting;
+            this.tabControl1.HandleCreated += tabControl1_HandleCreated;
+        }
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+        private const int TCM_SETMINTABWIDTH = 0x1300 + 49;
+        private void tabControl1_HandleCreated(object sender, EventArgs e)
+        {
+            SendMessage(this.tabControl1.Handle, TCM_SETMINTABWIDTH, IntPtr.Zero, (IntPtr)16);
+        }
+        private void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            if (e.TabPageIndex == this.tabControl1.TabCount - 1)
+                e.Cancel = true;
+        }
+        private void tabControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            var lastIndex = this.tabControl1.TabCount - 1;
+            if (this.tabControl1.GetTabRect(lastIndex).Contains(e.Location))
+            {
+                this.tabControl1.TabPages.Insert(lastIndex, "New Tab");
+                this.tabControl1.SelectedIndex = lastIndex;
+                this.tabControl1.TabPages[lastIndex].UseVisualStyleBackColor=true;
+            }
+            else
+            {
+                for (var i = 0; i < this.tabControl1.TabPages.Count; i++)
+                {
+                    var tabRect = this.tabControl1.GetTabRect(i);
+                    tabRect.Inflate(-2, -2);
+                    var closeImage = Properties.Resources.Close;
+                    var imageRect = new Rectangle(
+                        (tabRect.Right - closeImage.Width),
+                        tabRect.Top + (tabRect.Height - closeImage.Height) / 2,
+                        closeImage.Width,
+                        closeImage.Height);
+                    if (imageRect.Contains(e.Location))
+                    {
+                        this.tabControl1.TabPages.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+        }
+        private void tabControl1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            var tabPage = this.tabControl1.TabPages[e.Index];
+            var tabRect = this.tabControl1.GetTabRect(e.Index);
+            tabRect.Inflate(-2, -2);
+            if (e.Index == this.tabControl1.TabCount - 1)
+            {
+                var addImage = Properties.Resources.Add;
+                e.Graphics.DrawImage(addImage,
+                    tabRect.Left + (tabRect.Width - addImage.Width) / 2,
+                    tabRect.Top + (tabRect.Height - addImage.Height) / 2);
+            }
+            else
+            {
+                var closeImage = Properties.Resources.Close;
+                e.Graphics.DrawImage(closeImage,
+                    (tabRect.Right - closeImage.Width),
+                    tabRect.Top + (tabRect.Height - closeImage.Height) / 2);
+                TextRenderer.DrawText(e.Graphics, tabPage.Text, tabPage.Font,
+                    tabRect, tabPage.ForeColor, TextFormatFlags.Left);
+            }
+        }
+    }
+}
